8f419a793c7c9035559d40d200dc5ff45955c0ac
navjotsinghgill96376 <navy96376@gmail.com>
2026-07-11 20:46:29 +0530

    Transpose & Rotate Matrix

---
+++ Matrix/RotateMatrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i =0;i<n;i++){
+            int lo=0,hi=n-1;
+            while(lo<hi){
+                int temp = matrix[i][lo];
+                matrix[i][lo]=matrix[i][hi];
+                matrix[i][hi] = temp;
+                lo++;
+                hi--;
+            }
+        }
+
+    }
+}
